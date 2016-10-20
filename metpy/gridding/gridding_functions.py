c7b226ca21d2d25668fb1b0a061aeedce28ef807
Greg Blumberg <wblumberg@ou.edu>
2016-10-20 13:14:20 -0500

    Began adding stuff to support mutlipass and included avg station spacing using Delaunay

--- metpy/gridding/gridding_functions.py
+++ metpy/gridding/gridding_functions.py
@@ -14,12 +14,70 @@ from metpy.gridding import points
 
 from ..package_tools import Exporter
 
+import matplotlib.tri as tri
+
 exporter = Exporter(globals())
 
+def calc_avg_spacing(x, y, filter=False):
+    r"""Calculate the average spacing between the stations using nearest neighbor and Delaunay Triangulation
+
+    Parameters
+    ----------
+    x: ndarray
+        a list of the x points for the stations
+    y: ndarray
+        a list of the y points for the stations
+    filter: bool
+        filter out any outliers from the triangulation
+    
+    Returns
+    -------
+    avg_spacing: float
+        the average spacing (nearest neighbor)
+    """
+    
+    triang = tri.Triangulation(x,y)
+    for edge in p.edges:
+        edge_length.append(np.sqrt(np.power(x[edge[0]] - x[edge[1]],2) + np.power(y[edge[0]] - y[edge[1]], 2)))
+    if filter is True:
+        per = np.percentile(edge_length,(10,90))
+        i = np.where((edge_length > per[0]) & (edge_length < per[1]))[0]
+    else:
+        i = np.arange(len(edge_length))
+
+    avg_spacing = np.mean(edge_length[i])
+
+    return avg_spacing
+
+def estimate_grid_dx(x, y, avg_spacing=None):
+    r"""Calculate the recommended grid spacing using the 
+        Koch et al. 1982 recommendations.
+        
+        Parameters
+        ----------
+        x: ndarray
+            a list of the x points for the stations
+        y: ndarray
+            a list of the y points for the stations
+        avg_spacing: float
+            the average spacing between stations
+                
+        Returns
+        -------
+        None
+    """
+    if avg_spacing is None:
+        avg_spacing = calc_avg_spacing(x, y)
+    #avg_spacing = np.mean((cdist(list(zip(x, y)), list(zip(x, y))))) 
+    lower_bound = avg_spacing * (1./3.)
+    upper_bound = avg_spacing * 0.5
+    print "Recommended grid spacing is between:", round(lower_bound, 2), "and", round(upper_bound), "km"
 
 def calc_kappa(spacing, kappa_star=5.052):
     r"""Calculate the kappa parameter for barnes interpolation.
 
+    From Koch et al. 1982, JAMC (Eq. 13)
+    
     Parameters
     ----------
     spacing: float
@@ -131,12 +189,55 @@ def remove_repeat_coordinates(x, y, z):
 
     return x_, y_, z_
 
+def define_grid(x,y,hres,buffer,lbound,rbound,tbound,bbound):
+    r"""Define the grid for an analysis.
+
+    Parameters
+    ----------
+    x: float
+        x coordinate
+    y: float
+        y coordinate
+    hres: float
+        The horizontal resolution of the generated grid. Default 50000 meters.
+    buffer: float
+        How many meters to add to the bounds of the grid. Default 1000 meters.
+    lbound : float
+        number of meters to trim off of left side of box
+    rbound : float
+        number of meters to trim off of right side of box
+    tbound : float
+        number of meters to trim off the top part of the box
+    bbound : float
+        number of meters to trim off of the bottom part of the box
+
+    Returns
+    -------
+    grid_x: (N, 2) ndarray
+        Meshgrid for the resulting interpolation in the x dimension
+    grid_y: (N, 2) ndarray
+        Meshgrid for the resulting interpolation in the y dimension ndarray
+    img: (M, N) ndarray
+        2-dimensional array representing the interpolated values for each grid.
+    """
+
+
+    grid_x, grid_y = points.generate_grid(hres, points.get_boundary_coords(x, y),
+                                             buffer)
+    grid_x, grid_y = points.trim_grid(grid_x, grid_y, lbound, rbound, tbound, bbound)
+    
+    return (grid_x, grid_y)
+
+
+def multi_pass(x, y, z, grid, interp_type, min_neighbors, gamma, kappa_star=5.052, ave_spacing, search_radius):
+    r"""
+    """
+    return
+        
 
 @exporter.export
-def interpolate(x, y, z, interp_type='linear', hres=50000,
-                buffer=1, minimum_neighbors=3, gamma=0.25,
-                kappa_star=5.052, search_radius=None, rbf_func='linear',
-                lbound=0, rbound=0, tbound=0, bbound=0, rbf_smooth=0):
+def interpolate(x, y, z, guess, grid, interp_type='linear', minimum_neighbors=3, gamma=0.25,
+                kappa_star=5.052, ave_spacing=None, search_radius=None, rbf_func='linear',rbf_smooth=0):
     r"""Interpolate given (x,y), observation (z) pairs to a grid based on given parameters.
 
     Parameters
@@ -146,16 +247,16 @@ def interpolate(x, y, z, interp_type='linear', hres=50000,
     y: float
         y coordinate
     z: float
-        observation value
+        observation value (or deviation)
+    guess:(M, N) ndarray
+        a first guess grid with the same dimensions as the grid variable
+    grid: tuple
+        a defined grid with the x and y points
     interp_type: str
         What type of interpolation to use. Available options include:
         1) "linear", "nearest", "cubic", or "rbf" from Scipy.interpolate.
         2) "natural_neighbor", "barnes", or "cressman" from Metpy.mapping .
         Default "linear".
-    hres: float
-        The horizontal resolution of the generated grid. Default 50000 meters.
-    buffer: float
-        How many meters to add to the bounds of the grid. Default 1000 meters.
     minimum_neighbors: int
         Minimum number of neighbors needed to perform barnes or cressman interpolation for a
         point. Default is 3.
@@ -164,6 +265,8 @@ def interpolate(x, y, z, interp_type='linear', hres=50000,
     kappa_star: float
         Response parameter for barnes interpolation, specified nondimensionally
         in terms of the Nyquist. Default 5.052
+    ave_spacing: float
+        The average spacing between the stations.
     search_radius: float
         A search radius to use for the barnes and cressman interpolation schemes.
         If search_radius is not specified, it will default to the average spacing of
@@ -173,15 +276,7 @@ def interpolate(x, y, z, interp_type='linear', hres=50000,
         Options include: 'multiquadric', 'inverse', 'gaussian', 'linear', 'cubic',
         'quintic', and 'thin_plate'. Defualt 'linear'. See scipy.interpolate.Rbf for more
         information.
-    lbound : float
-        number of meters to trim off of left side of box
-    rbound : float
-        number of meters to trim off of right side of box
-    tbound : float
-        number of meters to trim off the top part of the box
-    bbound : float
-        number of meters to trim off of the bottom part of the box
-    rbf_smooth: float
+   rbf_smooth: float
         Smoothing value applied to rbf interpolation.  Higher values result in more smoothing.
 
     Returns
@@ -194,9 +289,7 @@ def interpolate(x, y, z, interp_type='linear', hres=50000,
         2-dimensional array representing the interpolated values for each grid.
     """
 
-    grid_x, grid_y = points.generate_grid(hres, points.get_boundary_coords(x, y),
-                                          buffer)
-    grid_x, grid_y = points.trim_grid(grid_x, grid_y, lbound, rbound, tbound, bbound)
+    grid_x, grid_y = grid
 
     if interp_type in ['linear', 'nearest', 'cubic']:
         points_zip = np.array(list(zip(x, y)))
@@ -206,9 +299,10 @@ def interpolate(x, y, z, interp_type='linear', hres=50000,
         img = interpolation.natural_neighbor(x, y, z, grid_x, grid_y)
 
     elif interp_type in ['cressman', 'barnes']:
-
-        ave_spacing = np.mean((cdist(list(zip(x, y)), list(zip(x, y)))))
-
+        if ave_spacing is None:
+            ave_spacing = calc_avg_spacing(x, y, filter=False)
+        #ave_spacing = np.mean((cdist(list(zip(x, y)), list(zip(x, y)))))
+        
         if search_radius is None:
             search_radius = ave_spacing
 
@@ -219,9 +313,10 @@ def interpolate(x, y, z, interp_type='linear', hres=50000,
                                                  kind=interp_type)
 
         elif interp_type == 'barnes':
-
+            
+        
             kappa = calc_kappa(ave_spacing, kappa_star)
-            img = interpolation.inverse_distance(x, y, z, grid_x, grid_y, search_radius,
+            img = guess + interpolation.inverse_distance(x, y, z, grid_x, grid_y, search_radius,
                                                  gamma, kappa, min_neighbors=minimum_neighbors,
                                                  kind=interp_type)
 
